4d5f2f50d2e21f42ae2cb1ad5c26e23565ca2cd3
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-12-12 12:38:16 +0800

    add public tests. get error

--- src/test/java/com/thoughtworks/MarsRover/test/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/MarsRover/test/MarsRoverTest.java
@@ -33,4 +33,36 @@ public class MarsRoverTest {
         assertEquals(solution.getOrientation(), roverOrientation);
     }
 
+    @Test
+    public void firstPublicCase() throws Exception {
+        int x = 5;
+        int y = 5;
+        int roverX = 1;
+        int roverY = 2;
+        char roverOrientation = 'N';
+        String instructions = "LMLMLMLMM";
+
+        RoverData solution = marsRoverApplication.solve(x, y, roverX, roverY, roverOrientation, instructions);
+
+        assertEquals(solution.getX(), 1);
+        assertEquals(solution.getY(), 3);
+        assertEquals(solution.getOrientation(), 'N');
+    }
+
+    @Test
+    public void secondPublicCase() throws Exception {
+        int x = 5;
+        int y = 5;
+        int roverX = 3;
+        int roverY = 3;
+        char roverOrientation = 'E';
+        String instructions = "MMRMMRMRRM";
+
+        RoverData solution = marsRoverApplication.solve(x, y, roverX, roverY, roverOrientation, instructions);
+
+        assertEquals(solution.getX(), 5);
+        assertEquals(solution.getY(), 1);
+        assertEquals(solution.getOrientation(), 'E');
+    }
+
 }
