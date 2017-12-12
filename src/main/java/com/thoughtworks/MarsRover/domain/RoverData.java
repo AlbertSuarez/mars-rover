329f38276b927dc08b5c266ccadcb76ceb23981b
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-12-12 12:33:21 +0800

    setup code. add first test: no instructions

---
+++ src/main/java/com/thoughtworks/MarsRover/domain/RoverData.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.MarsRover.domain;
+
+public class RoverData {
+
+    private int x;
+    private int y;
+    private char orientation;
+
+    public RoverData(int x, int y, char orientation) {
+        this.x = x;
+        this.y = y;
+        this.orientation = orientation;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public char getOrientation() {
+        return orientation;
+    }
+
+    public void setOrientation(char orientation) {
+        this.orientation = orientation;
+    }
+
+}
--- src/test/java/com/thoughtworks/MarsRover/test/MarsRoverTest.java
+++ src/test/java/com/thoughtworks/MarsRover/test/MarsRoverTest.java
@@ -1,9 +1,36 @@
 package com.thoughtworks.MarsRover.test;
 
+import com.thoughtworks.MarsRover.MarsRoverApplication;
+import com.thoughtworks.MarsRover.domain.RoverData;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(JUnit4.class)
 public class MarsRoverTest {
+
+    private static MarsRoverApplication marsRoverApplication;
+
+    public MarsRoverTest() {
+        marsRoverApplication = new MarsRoverApplication();
+    }
+
+    @Test
+    public void noInstructionsTest() throws Exception {
+        int x = 5;
+        int y = 5;
+        int roverX = 0;
+        int roverY = 0;
+        char roverOrientation = 'N';
+        String instructions = "";
+
+        RoverData solution = marsRoverApplication.solve(x, y, roverX, roverY, roverOrientation, instructions);
+
+        assertEquals(solution.getX(), roverX);
+        assertEquals(solution.getY(), roverY);
+        assertEquals(solution.getOrientation(), roverOrientation);
+    }
 
 }
