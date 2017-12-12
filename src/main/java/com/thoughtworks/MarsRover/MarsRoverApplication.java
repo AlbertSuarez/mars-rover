aa79e6731a39ba7f7a240fc2f6c575b8b43ee77b
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-12-12 12:10:42 +0800

    setup project schema

---
+++ src/main/java/com/thoughtworks/MarsRover/MarsRoverApplication.java
@@ -0,0 +1,5 @@
+package com.thoughtworks.MarsRover;
+
+public class MarsRoverApplication {
+
+}
---
+++ src/test/java/com/thoughtworks/MarsRover/test/MarsRoverTest.java
@@ -0,0 +1,9 @@
+package com.thoughtworks.MarsRover.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class MarsRoverTest {
+
+}
