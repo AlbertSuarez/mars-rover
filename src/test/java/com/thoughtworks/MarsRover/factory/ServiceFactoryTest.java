782dc90c8bc90f8b5336844aa18ffb43efb43bfd
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-12-16 18:00:16 +0800

    add serviceFactory tests. tests passed

--- src/test/java/com/thoughtworks/MarsRover/factory/ServiceFactoryTest.java
+++ src/test/java/com/thoughtworks/MarsRover/factory/ServiceFactoryTest.java
@@ -1,11 +1,26 @@
 package com.thoughtworks.MarsRover.factory;
 
+import com.thoughtworks.MarsRover.service.MarsRoverService;
+import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
+import static org.junit.Assert.assertNotNull;
 
 @RunWith(JUnit4.class)
 public class ServiceFactoryTest {
 
+    private MarsRoverService marsRoverService;
 
+    @Before
+    public void before() {
+        this.marsRoverService = ServiceFactory.getMarsRoverService();
+    }
+
+    @Test
+    public void noEmptyController() throws Exception {
+        assertNotNull(this.marsRoverService);
+    }
 
 }
