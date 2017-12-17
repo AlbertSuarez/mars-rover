2e28d26ffdd84b079f24828ca82e1caadaafecac
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-12-17 14:23:54 +0800

    optimize imports

--- src/test/java/com/thoughtworks/MarsRover/controller/PlateauControllerTest.java
+++ src/test/java/com/thoughtworks/MarsRover/controller/PlateauControllerTest.java
@@ -6,9 +6,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 @RunWith(JUnit4.class)
 public class PlateauControllerTest {
