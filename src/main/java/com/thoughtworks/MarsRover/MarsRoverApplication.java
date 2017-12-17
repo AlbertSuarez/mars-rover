a2f335882e37acc9275808b819ca0ed764751ee9
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-12-17 13:04:11 +0800

    implement main function with new architechture

--- src/main/java/com/thoughtworks/MarsRover/MarsRoverApplication.java
+++ src/main/java/com/thoughtworks/MarsRover/MarsRoverApplication.java
@@ -1,6 +1,8 @@
 package com.thoughtworks.MarsRover;
 
 import com.thoughtworks.MarsRover.domain.RoverRobot;
+import com.thoughtworks.MarsRover.factory.ServiceFactory;
+import com.thoughtworks.MarsRover.service.MarsRoverService;
 
 import java.util.Scanner;
 
@@ -11,26 +13,28 @@ public class MarsRoverApplication {
      * @param args arguments
      */
     @SuppressWarnings("InfiniteLoopStatement")
-    public static void main(String[] args) {
-//        Scanner scanner = new Scanner(System.in);
-//        int x = scanner.nextInt();
-//        int y = scanner.nextInt();
-//
-//        RoverRobot roverRobot;
-//        while (true) {
-//            int roverX = scanner.nextInt();
-//            int roverY = scanner.nextInt();
-//            char roverOrientation = scanner.next().charAt(0);
-//            String instructions = scanner.next();
-//
-//            // TODO Complete code
-//
-//            System.out.print(roverRobot.getX());
-//            System.out.print(' ');
-//            System.out.print(roverRobot.getY());
-//            System.out.print(' ');
-//            System.out.println( roverRobot.getOrientation());
-//        }
+    public static void main(String[] args) throws Exception {
+        MarsRoverService marsRoverService = ServiceFactory.getMarsRoverService();
+
+        Scanner scanner = new Scanner(System.in);
+        int x = scanner.nextInt();
+        int y = scanner.nextInt();
+
+        RoverRobot roverRobot;
+        while (true) {
+            int roverX = scanner.nextInt();
+            int roverY = scanner.nextInt();
+            char roverOrientation = scanner.next().charAt(0);
+            String instructions = scanner.next();
+
+            roverRobot = marsRoverService.solve(x, y, roverX, roverY, roverOrientation, instructions);
+
+            System.out.print(roverRobot.getX());
+            System.out.print(' ');
+            System.out.print(roverRobot.getY());
+            System.out.print(' ');
+            System.out.println( roverRobot.getOrientation());
+        }
     }
 
 }
