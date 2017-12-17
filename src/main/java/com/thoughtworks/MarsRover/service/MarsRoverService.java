958532d161f99a75e8fbed1581bcdc715e6c5fa2
albert <albert.suarez.molgo@est.fib.upc.edu>
2017-12-17 12:30:36 +0800

    add public cases on MarsRoverService test

--- src/main/java/com/thoughtworks/MarsRover/service/MarsRoverService.java
+++ src/main/java/com/thoughtworks/MarsRover/service/MarsRoverService.java
@@ -4,6 +4,17 @@ import com.thoughtworks.MarsRover.domain.RoverRobot;
 
 public interface MarsRoverService {
 
-    RoverRobot solve(int x, int y, int roverX, int roverY, int roverOrientation, String instructions);
+    /**
+     * Solve Mars Rover problem with a single rover using parameters
+     * @param x upper-right x coordinates of the plateau
+     * @param y upper-right y coordinates of the plateau
+     * @param roverX x start coordinates of the rover
+     * @param roverY y start coordinates of the rover
+     * @param roverOrientation start orientation of the rover (N, S, W, E)
+     * @param instructions character string with all movements (L, R, M)
+     * @return rover data (x, y and orientation) at the end of the instructions
+     * @throws Exception error when some input is incorrect
+     */
+    RoverRobot solve(int x, int y, int roverX, int roverY, int roverOrientation, String instructions) throws Exception;
 
 }
--- src/test/java/com/thoughtworks/MarsRover/service/MarsRoverServiceTest.java
+++ src/test/java/com/thoughtworks/MarsRover/service/MarsRoverServiceTest.java
@@ -1,15 +1,20 @@
 package com.thoughtworks.MarsRover.service;
 
+import com.thoughtworks.MarsRover.domain.RoverRobot;
 import com.thoughtworks.MarsRover.factory.ServiceFactory;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(JUnit4.class)
 public class MarsRoverServiceTest {
+
+    private static final int x = 5;
+    private static final int y = 5;
 
     private MarsRoverService marsRoverService;
 
@@ -21,6 +26,56 @@ public class MarsRoverServiceTest {
     @Test
     public void noEmptyService() throws Exception {
         assertNotNull(this.marsRoverService);
+    }
+
+    @Test
+    public void solveWithoutInstructions() throws Exception {
+        final int roverX = 0;
+        final int roverY = 0;
+        final char roverOrientation = 'N';
+        final String instructions = "";
+
+        RoverRobot solution = this.marsRoverService.solve(x, y, roverX, roverY, roverOrientation, instructions);
+
+        assertEquals(solution.getX(), roverX);
+        assertEquals(solution.getY(), roverY);
+        assertEquals(solution.getOrientation(), roverOrientation);
+    }
+
+    @Test
+    public void solveWithInstructionsAndWithNorthOrientation() throws Exception {
+        final int roverX = 1;
+        final int roverY = 2;
+        final char roverOrientation = 'N';
+        final String instructions = "LMLMLMLMM";
+
+        final int expectedX = 1;
+        final int expectedY = 3;
+        final char expectedOrientation = 'N';
+
+        RoverRobot solution = this.marsRoverService.solve(x, y, roverX, roverY, roverOrientation, instructions);
+
+        assertEquals(solution.getX(), expectedX);
+        assertEquals(solution.getY(), expectedY);
+        assertEquals(solution.getOrientation(), expectedOrientation);
+    }
+
+    @Test
+    public void solveWithInstructionsAndWithEastOrientation() throws Exception {
+        final int roverX = 3;
+        final int roverY = 3;
+        final char roverOrientation = 'E';
+        final String instructions = "MMRMMRMRRM";
+
+        final int expectedX = 5;
+        final int expectedY = 1;
+        final char expectedOrientation = 'E';
+
+        RoverRobot solution = this.marsRoverService.solve(x, y, roverX, roverY, roverOrientation, instructions);
+
+        assertEquals(solution.getX(), expectedX);
+        assertEquals(solution.getY(), expectedY);
+        assertEquals(solution.getOrientation(), expectedOrientation);
     }
 
 }
